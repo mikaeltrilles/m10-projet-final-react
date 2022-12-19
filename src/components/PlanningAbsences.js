98fe544a10d740d5396aeb6a9e9c612423e5445e
Mikael Trilles <11914687-mikaeltrilles@users.noreply.gitlab.com>
2022-12-19 08:18:54 +0100

    # Installation sz full callendar

--- src/components/PlanningAbsences.js
+++ src/components/PlanningAbsences.js
@@ -1,9 +1,26 @@
 import React from 'react';
+// import calendar
+// import { Calendar } from '@fullcalendar/core';
+// import dayGridPlugin from '@fullcalendar/daygrid';
+// import timeGridPlugin from '@fullcalendar/timegrid';
+// import listPlugin from '@fullcalendar/list';
 
 const PlanningAbsences = () => {
+
+    // let calendar = new Calendar(calendarEl, {
+    //     plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    //     initialView: 'dayGridMonth',
+    //     headerToolbar: {
+    //         left: 'prev,next today',
+    //         center: 'title',
+    //         right: 'dayGridMonth,timeGridWeek,listWeek'
+    //     }
+    // });
+    // calendar.render();
     return (
         <div>
             <h1>PLANNING ABSENCES</h1>
+
         </div>
     );
 };
